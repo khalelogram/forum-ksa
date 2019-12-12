c10c1b6b44f587c042f2933102bddf154d56f4aa
cbz2810 <cbz2810@gmail.com>
2019-12-12 15:47:17 +0800

    created separate files for header, navbar, and banner for includes

---
+++ banner.php
@@ -0,0 +1,6 @@
+  <header class="bg-primary text-white">
+    <div class="container text-center">
+      <h1>Welcome to KSA Forum</h1>
+      <p class="lead">A place of discussion for KAL Skills Akademie Students</p>
+    </div>
+  </header>
